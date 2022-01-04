0219b1182a00783cf38194c9d331ff53578a9483
alex-sandro <alex.afortiori@gmail.com>
2022-01-04 00:36:32 -0300

    Dados para teste

---
+++ gestao-de-escalas/src/data.js
@@ -0,0 +1,19 @@
+const funcionario = [
+    {
+        id: 1,
+        img:'',
+        nome: 'Frank',
+        sobrenome: 'Aguiar',
+        dataEntrada: '03/01/2022',
+        entrada: '08:00',
+        dataSaida: '03/01/2022',
+        saida: '10:31',
+        escala: '08:10 - 14:35',
+        pausaLanche: '12:00 - 12:25',
+        pausaDescanso: '13:30 - 13:40',
+        tempoStatus: '03:04',
+        status: 'Livre'
+    }
+];
+
+export default funcionario;
