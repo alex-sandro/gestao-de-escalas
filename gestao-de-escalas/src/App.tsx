21d101415ea15a5a055da4e0934e88dfb28a1d4f
alex-sandro <alex.afortiori@gmail.com>
2022-01-04 00:45:10 -0300

    Exibindo o componente da Escala

--- gestao-de-escalas/src/App.tsx
+++ gestao-de-escalas/src/App.tsx
@@ -1,36 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import userList from './data.js';
+import UserEscala from './components/UserEscala';
 
-function App() {
+const App = () => {
+
+  const [users, setUsers] = useState(userList);
+
   return (
-    <div className="App">
-        <h1>Gestão de Escalas</h1>
-        <div className="App-panel panel-title">
-          <div>Funcionário</div>
-          <div>Entrada</div>
-          <div>Saída</div>
-          <div>Escala</div>
-          <div>Pausa lanche</div>
-          <div>Pausa Descanço</div>
-          <div>status</div>
+    <div className="container">
+        <h1>Gestão de escalas</h1>
+        <div className="card">
+          <div className='card-body'>
+            <UserEscala users={users}/>
+          </div> 
         </div>
-        <div className="App-panel">
-          <div>Nome Sobrenome</div>
-          <div>01/01/2022
-            <div>08:10</div>
-          </div>
-          <div>01/01/2022
-            <div>10:31</div>
-          </div>  
-          <div>08:10 - 14:35</div>
-          <div>12:00 - 12:25</div>
-          <div>13:30 - 13:40</div>  
-          <div>03:04
-            <div> <small>Livre</small> </div>  
-          </div>       
-        </div> 
-    </div>
-  );
-}
+    </div>           
+)}
 
 export default App;
